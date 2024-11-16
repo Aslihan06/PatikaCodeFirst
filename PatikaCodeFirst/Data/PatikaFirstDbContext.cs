b57f3b5b1bf980a27a733538938e203df84decbe
Aslıhan ESEN <aslyqc14@gmail.com>
2024-11-17 00:31:54 +0300

    Proje dosyası ekle.

---
+++ PatikaCodeFirst/Data/PatikaFirstDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PatikaCodeFirst.Models;
+using System.Collections.Generic;
+using System.Runtime.Remoting.Contexts;
+
+namespace PatikaCodeFirst.Data
+{
+    public class PatikaFirstDbContext : DbContext
+    {
+        public PatikaFirstDbContext(DbContextOptions<PatikaFirstDbContext> options) : base(options) { }
+
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Game> Games { get; set; }
+    }
+}
+
---
+++ PatikaCodeFirst/Models/Movie.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PatikaCodeFirst.Models
+{
+    public class Movie
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Title { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Genre { get; set; }
+
+        [Range(1900, 2100)]
+        public int ReleaseYear { get; set; }
+    }
+}
+
